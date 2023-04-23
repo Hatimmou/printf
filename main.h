34f2bf39a145418256ce85679e0fa78ce81c7bc7
Hatimmou <mounaji97@gmail.com>
2023-04-22 20:06:21 -0700

    printf

--- main.h
+++ main.h
@@ -81,8 +81,7 @@ int print_pointer(va_list types, char buffer[],
 
 /* Funciotns to handle other specifiers */
 int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
+int get_width(const char *format, int *i, va_list list);int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
 /*Function to print string in reverse*/
